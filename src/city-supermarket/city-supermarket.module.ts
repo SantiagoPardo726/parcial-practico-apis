3d8be6b60446971dfdfb50516a6a4a61d165285b
SantiagoPardo726 <69635343+SantiagoPardo726@users.noreply.github.com>
2023-11-17 23:56:16 -0500

    Add CitySupermarketController to CitySupermarketModule

--- src/city-supermarket/city-supermarket.module.ts
+++ src/city-supermarket/city-supermarket.module.ts
@@ -3,9 +3,11 @@ import { CitySupermarketService } from './city-supermarket.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityEntity } from 'src/city/city.entity';
 import { SupermarketEntity } from 'src/supermarket/supermarket.entity';
+import { CitySupermarketController } from './city-supermarket.controller';
 
 @Module({
   providers: [CitySupermarketService],
   imports: [TypeOrmModule.forFeature([CityEntity,SupermarketEntity])],
+  controllers: [CitySupermarketController],
 })
 export class CitySupermarketModule {}
